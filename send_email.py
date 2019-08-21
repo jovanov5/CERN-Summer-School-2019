9fdc11219ad3fbdbd0ba82c143f65a3d2d392701
Jovan Jovanovic <jjovanovic996@gmail.com>
2019-08-21 20:28:57 +0200

    EMAILING implemented in Ca Experiment

---
+++ send_email.py
@@ -0,0 +1,69 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+import os.path
+
+
+def send_email(comp_time, fig1='', fig2='', fig3=''):
+
+    H = int(comp_time / 3600)
+    M = int((comp_time - H * 3600) / 60)
+    S = int(comp_time - 3600 * H - 60 * M)
+
+    email = 'srvrinformer@gmail.com'
+    password = '&Ab012_8Zp2!'
+    send_to_email = 'jjovanovic996@gmail.com'
+    subject = 'Simulation Completed'
+    message = 'Time taken: ' + str(H) + ':' + str(M) + ':' + str(S)
+
+    msg = MIMEMultipart()
+    msg['From'] = email
+    msg['To'] = send_to_email
+    msg['Subject'] = subject
+
+    msg.attach(MIMEText(message, 'plain'))
+
+    # Setup the attachment
+    file_location = os.path.dirname(os.path.realpath(__file__))+'\\' + fig1
+    filename = os.path.basename(file_location)
+    attachment = open(file_location, "rb")
+    part = MIMEBase('application', 'octet-stream')
+    part.set_payload(attachment.read())
+    encoders.encode_base64(part)
+    part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+    # Attach the attachment to the MIMEMultipart object
+    msg.attach(part)
+
+    # Setup the attachment
+    file_location = os.path.dirname(os.path.realpath(__file__))+'\\' + fig2
+    filename = os.path.basename(file_location)
+    attachment = open(file_location, "rb")
+    part = MIMEBase('application', 'octet-stream')
+    part.set_payload(attachment.read())
+    encoders.encode_base64(part)
+    part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+    # Attach the attachment to the MIMEMultipart object
+    msg.attach(part)
+
+    # Setup the attachment
+    file_location = os.path.dirname(os.path.realpath(__file__))+'\\' + fig3
+    filename = os.path.basename(file_location)
+    attachment = open(file_location, "rb")
+    part = MIMEBase('application', 'octet-stream')
+    part.set_payload(attachment.read())
+    encoders.encode_base64(part)
+    part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+    # Attach the attachment to the MIMEMultipart object
+    msg.attach(part)
+
+
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(email, password)
+    text = msg.as_string()
+    server.sendmail(email, send_to_email, text)
+    server.quit()
+
+    return 0
